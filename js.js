1bd4e80a69ffc9d3739e9a6ac41feb7cba647797
jordondoug2019 <jdouglas2015@gmail.com>
2019-09-12 16:21:15 -0500

    CW Final

--- js.js
+++ js.js
@@ -4,9 +4,31 @@ DO NOT ALTER THE EXISTING HTML TO DO THIS.  WRITE ONLY CSS!
 
 
 /* Give everything with the class 'hello' a white background when you click on one of them*/
+$(".hello").click(function ()
+{
+    console.log("click");
+    $(".hello").css("background-color","yellow")
+});
 
+// console.log($("hello"));
 
 /* When you mouse over the "Selectors Exercise" element, change the height&width to 20px. When you mouse off of it change it back */
+$("h1").mouseover(function ()
+{
+    $("h1").css("height","20px");
+    $("h1").css("width","20px")
+});
+$("h1").mouseout(function ()
+{
+    $("h1").css("height","40px");
+    $("h1").css("width","900px");
+});
 
 // CHALLENGE
 /* Each time the sentence "I am a paragraph with a class" is clicked, add a "." to the end of the sentence. */
+
+$("p.hello").click(function ()
+{
+    console.log("click");
+    $("p.hello").append(".")
+});
